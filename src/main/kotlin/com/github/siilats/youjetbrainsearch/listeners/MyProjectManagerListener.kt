730c3fc93c8de6226ee663025c961e306b243d90
GitHub Action <action@github.com>
2022-09-13 22:00:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/siilats/youjetbrainsearch/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/siilats/youjetbrainsearch/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.siilats.youjetbrainsearch.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.siilats.youjetbrainsearch.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/siilats/youjetbrainsearch/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/siilats/youjetbrainsearch/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.siilats.youjetbrainsearch.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.siilats.youjetbrainsearch.MyBundle
 
 class MyApplicationService {
 
