4c90bc9867264fa8476dbd0b65d4ef5eaf390033
keith siilats <keith@siilats.com>
2022-10-06 20:37:36 +0300

    Update TelemetryNotifications.java

--- src/main/java/com/redhat/devtools/intellij/telemetry/ui/TelemetryNotifications.java
+++ src/main/java/com/redhat/devtools/intellij/telemetry/ui/TelemetryNotifications.java
@@ -44,7 +44,7 @@ public class TelemetryNotifications {
 
     public void queryUserConsent() {
         Notification notification = group.createNotification(
-                "Help Red Hat improve its extensions by allowing them to collect anonymous usage data. " +
+                "Help You.com improve its extensions by allowing them to collect anonymous usage data. " +
                 "Read our <a href=\"https://developers.redhat.com/article/tool-data-collection\">privacy statement</a> " +
                 "and learn how to <a href=\"\">opt out</a>.",
         NotificationType.INFORMATION);
